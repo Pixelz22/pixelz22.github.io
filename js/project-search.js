18889025335f855d01c9c7c26ee4c32d2d2e2f41
Pixelz22 <tljohnson1225@gmail.com>
2025-10-05 21:19:35 -0400

    Page's unfinished status now shown on listing

--- js/project-search.js
+++ js/project-search.js
@@ -1,7 +1,7 @@
 let listingTemplate = Handlebars.compile(`
 {{#each projects}}
-    <project>
-        <a href="{{this.link}}">
+    <project {{#if this.link}}class="active"{{else}}class="inactive"{{/if}}>
+        <a {{#if this.link}} href="{{this.link}}" {{/if}}>
             <div style="box-sizing: border-box; padding: 1em 1em; height: 100%;">
                 <h3>{{this.name}}</h3>
                 <p>{{{this.summary}}}</p>
