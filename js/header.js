018c2051b402ffbae105c9262b17c746dd1fc507
Pixelz22 <tljohnson1225@gmail.com>
2025-10-13 13:05:06 -0400

    major rework of CSS structure, added contact info

--- js/header.js
+++ js/header.js
@@ -1,15 +1,17 @@
 let headerTemplate = Handlebars.compile(`
-    <div class="sm-flex-v sm-flex-place-items-center">
-        <div style="font-family: 'JetBrains Mono', monospace;
-                        font-weight: bold;
-                        font-size: 2em;">Tyler Johnson</div>
-        <div>Coding and Composing with a Creative Soul</div>
+    <div class="flex place-items-center justify-content-space w-full sm-flex-v sm-place-content-center">
+        <div class="sm-flex-v sm-place-items-center">
+            <div style="font-family: 'JetBrains Mono', monospace;
+                            font-weight: bold;
+                            font-size: 2em;">Tyler Johnson</div>
+            <div>Coding and Composing with a Creative Soul</div>
+        </div>
+        <nav>
+            <a class="navlink" href="/">About</a>
+            <a class="navlink" href="/projects/">Explore Projects</a>
+            <!-- <a class="navlink" href="/resume">Resume</a> -->
+        </nav>
     </div>
-    <nav>
-        <a class="navlink" href="/">About</a>
-        <a class="navlink" href="/projects/">Explore Projects</a>
-        <!-- <a class="navlink" href="/resume">Resume</a> -->
-    </nav>
 `);
 
 document.getElementsByTagName('header')[0].innerHTML = headerTemplate();
